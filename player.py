b8c5d499ba482ddaeb6900725d5a2d588cb864a2
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2021-04-16 22:46:39 -0700

    fierro!

--- player.py
+++ player.py
@@ -1,4 +1,39 @@
 
 
 class Player:
-    
+    def __init__(self,  _name,  _politica,  _educacion,  _popularidad, _mascara):
+        self.name = name
+        self.politica = _politica
+        self.educacion = _educacion
+        self.popularidad = _popularidad
+        self.mascara = _mascara
+
+    def getPol(self):
+        return self.politica
+
+    def getEdu():
+        return self.educacion
+
+    def getPop():
+        return self.popularidad
+
+    def calculatePhil():
+        philList = {"Dios": "teologica", 
+                    "'Griegos'": "naturalista", "Democrito":"democratico",
+                    "Odin":"Mitologica",
+                    "Socrates":"socratico",
+                    "Platon":"platonico",
+                    'Papa':'Edad Media',
+                    "Davinci":"Renacimiento",
+                    "Descartes":"Dualista",
+                    "Locke":"Liberalimo",
+                    "Berkeley":"inmaterialismo",
+                    "Kant":"idealismo",
+                    "":"Romaticismo"}
+
+
+def main():
+    print('hello world!')
+
+
+main()
