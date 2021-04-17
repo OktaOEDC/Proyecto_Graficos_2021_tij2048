a2c8b42ba77f4f86ca4ec5755736fa5a02672ca0
Omar Duran <54329113+OktaOEDC@users.noreply.github.com>
2021-04-17 11:32:29 -0700

    Main menu complete
    Menu principal terminado, recuerden usar blender para mapear texturas

--- config/conf.prc
+++ config/conf.prc
@@ -3,3 +3,6 @@ window-title Tijuana 2033
 fullscreen false
 sync-video true
 show-frame-rate-meter true
+background-color 0.1 0.1 0.33 0.0
+load-display pandadx9
+aux-display pandagl
--- main.py
+++ main.py
@@ -4,16 +4,15 @@ from direct.gui.OnscreenImage import OnscreenImage
 from direct.gui.DirectEntry import DirectEntry
 from direct.gui.DirectButton import DirectButton
 from panda3d.core import loadPrcFile
-from panda3d.core import MouseWatcher
-from panda3d.core import DisplayRegion
 from panda3d.core import WindowProperties
 from panda3d.core import TextNode
 from panda3d.core import Vec3
 from panda3d.core import TextFont
+from panda3d.core import PointLight
 from direct.gui.DirectGuiGlobals import WITHIN
 loadPrcFile("config/conf.prc")
 
-class test1(ShowBase):
+class Tijuana2033(ShowBase):
     def __init__(self):
         super().__init__()  
         self.playerName= "Player_1"
@@ -21,12 +20,18 @@ class test1(ShowBase):
         self.IsFullScreen = False
         self.disableMouse()
         ##
+        self.light_model = self.loader.loadModel('models/misc/sphere')
+        self.light_model.setScale(0.1, 0.1, 0.1)
+        self.light_model.reparentTo(self.render)
+        ##
+        plight = PointLight("plight")
+        plight.setColor((1,1,1,1))
+        self.plnp = self.render.attachNewNode(plight)
+        self.render.setLight(self.plnp)
 
-    def mouseOver(self, argumento):
-        ##self.scene.setHpr(120,230,30)  
-        rotation_interval = self.scene.hprInterval(10,Vec3(360,0,0))
+    def mouseOver(self, argumento): 
+        rotation_interval = self.fullScreenShape.hprInterval(10,Vec3(360,0,0))
         rotation_interval.start()
-        pass
 
     def getPlayerName(self):
         self.playerName = self.TextInsertNameInput.get()
@@ -41,16 +46,16 @@ class test1(ShowBase):
             props.fullscreen = True
             self.IsFullScreen = True
         self.win.requestProperties(props)
-        pass
 
     def Main_menu(self):
-        self.scene = self.loader.loadModel("models/Espejo_fullscreen.bam")
-        self.scene.reparentTo(self.render)
-        # Apply scale and position transforms on the model.
-        self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(-1.5, 6,-1)
-        ##Todo hacer prisma para modelo
-        ##imageObject = OnscreenImage(image='backgrounds/90sbg.jpg', pos=(0, 0, 0), scale=(1.78,1,1))
+        self.fullScreenShape = self.loader.loadModel("models/fullscreen_shape.bam")
+        self.fullScreenShape.reparentTo(self.render)
+        self.fullScreenShape.setScale(0.25, 0.25, 0.25)
+        self.fullScreenShape.setPos(-1.5, 6,-1 )
+        self.fondo = self.loader.loadModel("models/fondo_menu.bam")
+        self.fondo.reparentTo(self.render)
+        self.fondo.setScale(0.27,0.01,0.32)
+        self.fondo.setPos(0, 7, 0)
         font = loader.loadFont('./fonts/comic/comic.ttf')
         TextInsertName = OnscreenText(font=font, text='Ingrese su nombre:', pos=(-0.5, 0.02), scale=0.07, fg=(143/255,250/255,2/255,1))
         TextFullScreen = OnscreenText(font=font, text='Fulscreen:', pos=(-0.99, -0.67), scale=0.04, fg=(143/255,250/255,2/255,1))
@@ -59,5 +64,5 @@ class test1(ShowBase):
         self.InvisibleButton = DirectButton(text=("*"), scale=.05, pos=(-1, 5,-0.73),command=self.setFullScreen)
         self.InvisibleButton.bind(WITHIN, command = self.mouseOver)
 
-jueguito = test1()
-jueguito.run()
+Game = Tijuana2033()
+Game.run()
