021c0c3c4306eeac632cedf1a3d823738e608b7c
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2021-04-16 19:50:28 -0700

    folder font structure

--- textcrawl.py
+++ textcrawl.py
@@ -2,7 +2,7 @@ import logging;
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class textcrawl():
-
+    font = loader.Font('')
     def crawl_main():
         """This is the main function of the text crawl class
         """
@@ -15,3 +15,4 @@ text_data2=["Tijuana cedio a ser una democracia directa en 2025"];
 text_data3=["Dubai y China compraron McDonalds en 2026 debido al colapso espcultativo del mercado de gallos en Asia."];
 text_data4=["En 2024 los Estados Unidos prohibio en su enteridad la produccion de carnes a base de animales.","El SOYBOI PARTY ha remplazado al partido libertariano como la astilla de circulo politico en Washington "];
 text_data5=["Mandato numero 1984 permite el uso continuo en espacios publicos de mascaras de cobertura completa tras la pandemia de 2022 de covid-21"];
+text_titles_titles=["INJUSTICIA","INDIVIDUO","BUSQUEDA DE JUSTICIA","FALLA DE SISTEMA"];
