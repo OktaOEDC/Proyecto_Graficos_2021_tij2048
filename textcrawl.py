0536da1b08743e954183ac624988e6e64e17d12d
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2021-04-19 14:59:07 -0700

    text crawl yay!

--- textcrawl.py
+++ textcrawl.py
@@ -1,7 +1,9 @@
 import os
+from direct import task
 from direct.showbase.ShowBase import *
 from panda3d.core import *
 from direct.gui.OnscreenText import OnscreenText
+from direct.task import Task, TaskManagerGlobal
 import logging
 import time
 
@@ -32,34 +34,33 @@ class Textcrawl(ShowBase):
         # Apply scale and position transforms on the model.
         self.scene.setScale(1, 1, 1)
         self.scene.setPos(0, 25, 0)
-        self.make_text()
-
-
-    def make_text(self):
-        """AUN NO LO HE HECHO
-        """
-        #font = loader.loadFont('/fonts/comic/comicbd.ttf')
-        #font.setPageSize(512,512)
-        
+        self.do_method_later= taskMgr.doMethodLater(0,self.update,'update')
+                
+    def update(self, task):
         textposx=0
         textposy=-0.5
+        self.textdelay = 1
         for x in self.story:
             for y in x:
             
                 if y == 'TIJUANA 2033':
-                    pass
+                    logging.debug('esto se ignora')
                 else:
-                    textObject = OnscreenText(text=y, pos=(textposx,textposy),scale=0.05,  fg=[240,240,240,1], wordwrap=45, bg=[0,0,0,0.1])
+                    self.make_text= taskMgr.doMethodLater(self.textdelay,self.print_text,
+                                                                'print_text',extraArgs=[task,y,textposx,textposy])
+                    self.textdelay+=2
                     textposy += 0.1
             textposy+=0.05
-            
+        
+        return task.done
+    
+    
 
-    def print_text(self, task):
+    def print_text(self, task, y, textposx, textposy):
         """Esta funcion es con la que imprimire lentamente el text crawl en vez de todo del putaso
         """
-        
-        pass
-        
+        self.textObject = OnscreenText(text=y, pos=(textposx,textposy),scale=0.05,  fg=[240,240,240,1], wordwrap=45, bg=[0,0,0,0.1])
+        return task.done
     
 game = Textcrawl()
 game.run()
