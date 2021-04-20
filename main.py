0d01ae4126944104d2ee3b573bc8ed3407bc66d5
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2021-04-19 23:01:52 -0700

    vale verga

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
+import time
 import logging
 
 from direct.gui.DirectButton import DirectButton
-<<<<<<< HEAD
 from direct.gui.DirectEntry import DirectEntry
-=======
-from panda3d.core import Fog
-from panda3d.core import loadPrcFile
-from panda3d.core import WindowProperties
-from panda3d.core import TextNode
-from panda3d.core import Vec3
-from panda3d.core import TextFont
-from panda3d.core import PointLight
->>>>>>> 41a598963fcc8b82062a41293ac630927ed6bc20
 from direct.gui.DirectGuiGlobals import WITHIN
 from direct.gui.OnscreenImage import OnscreenImage
 from direct.gui.OnscreenText import OnscreenText
@@ -20,8 +11,8 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task, TaskManagerGlobal
 from direct.task.TaskManagerGlobal import taskMgr
 #from direct import *
-from panda3d.core import (PointLight, TextFont, TextNode, Vec3,
-                          WindowProperties, loadPrcFile, NodePath)
+from panda3d.core import (Fog, NodePath, PointLight, TextFont, TextNode, Vec3,
+                          WindowProperties, loadPrcFile)
 
 import player
 
@@ -31,35 +22,47 @@ loadPrcFile("config/conf.prc")
 text_titles_titles = ["INJUSTICIA", "INDIVIDUO",
                       "BUSQUEDA DE JUSTICIA", "FALLA DE SISTEMA"]
 
+taskAccumulator = 0
+
+
+def cleanUp(task):
+    global taskAccumulator
+    print("Task func has accumulated %d" % taskAccumulator)
+    # Reset the accumulator
+    taskAccumulator = 0
+
+    # A task that runs forever
+
+
+def taskFunc(task):
+    global taskAccumulator
+    taskAccumulator += 1
+    return task.cont
+
+
+def taskStop(task):
+    taskMgr.remove('Accumulator')
+
 
 class Tijuana2033(ShowBase):
     def __init__(self):
         super().__init__()
-        self.playerName = "Player_1"
+        self.playerName = ""
         self.IsFullScreen = False
+        self.blade_runner = player.Player(self.playerName)
         self.disableMouse()
         ##
-<<<<<<< HEAD
-        self.light_model = self.loader.loadModel('models/misc/sphere')
-        self.light_model.setScale(0.1, 0.1, 0.1)
-        self.light_model.reparentTo(self.render)
-        ##
-        plight = PointLight("plight")
-        plight.setColor((1, 1, 1, 1))
-        self.plnp = self.render.attachNewNode(plight)
-=======
         self.plight = PointLight("plight")
-        self.plight.setColor((1,1,1,1))
+        self.plight.setColor((1, 1, 1, 1))
         self.plnp = self.render.attachNewNode(self.plight)
-        ##self.plight.setAttenuation((1,0,1))
+        # self.plight.setAttenuation((1,0,1))
         ##self.plnp.setPos(0.33, 6,4)
->>>>>>> 41a598963fcc8b82062a41293ac630927ed6bc20
         self.render.setLight(self.plnp)
-        #self.Main_menu()
-        
-        taskMgr.add(self.Main_menu,'first',sort=1)
-        
-        
+        # self.Main_menu()
+
+        taskMgr.add(self.Main_menu, 'first', sort=1, uponDeath=cleanUp)
+        taskMgr.add(self.transition, 'trans', sort=2, uponDeath=cleanUp)
+
     def mouseOver(self, argumento):
         rotation_interval = self.fullScreenShape.hprInterval(
             10, Vec3(360, 0, 0))
@@ -67,17 +70,14 @@ class Tijuana2033(ShowBase):
 
     def getPlayerName(self):
         self.playerName = self.TextInsertNameInput.get()
-<<<<<<< HEAD
         self.blade_runner = player.Player(self.playerName)
         print('Hola', self.blade_runner.get_name(), 'bienvenido a TIJUANA 2033')
         # Remover el menu principal
-=======
-        self.blade_runner = player.Player(self.playerName) 
-        print('Hola',self.blade_runner.get_name(),'bienvenido a TIJUANA 2033')
-        ##Remover el menu principal
+        self.blade_runner = player.Player(self.playerName)
+        print('Hola', self.blade_runner.get_name(), 'bienvenido a TIJUANA 2033')
+        # Remover el menu principal
         self.render.clearLight()
         self.plnp.removeNode()
->>>>>>> 41a598963fcc8b82062a41293ac630927ed6bc20
         self.fondo.removeNode()
         self.fullScreenShape.removeNode()
         self.TextInsertName.removeNode()
@@ -85,35 +85,32 @@ class Tijuana2033(ShowBase):
         self.ButtonInsertName.removeNode()
         self.InvisibleButton.removeNode()
         self.TextFullScreen.removeNode()
-        
-<<<<<<< HEAD
-        taskMgr.myTask = taskMgr.doMethodLater(0, self.transition, 'transition')
-=======
         ###
+
+    def level1(self, task):
         self.Nivel1 = self.loader.loadModel("models/Campestre_light.bam")
         self.Nivel1.reparentTo(self.render)
         self.Nivel1.setScale(1, 1, 1)
-        self.Nivel1.setPos(0.33, 6,-1 )
-        self.camera.setPos(-0.66,2.5,0.55)
-        ##self.camera.setPos(0.33,6,5)
+        self.Nivel1.setPos(0.33, 6, -1)
+        self.camera.setPos(-0.66, 2.5, 0.55)
+        # self.camera.setPos(0.33,6,5)
         self.camera.lookAt(self.Nivel1)
-        self.background = OnscreenImage(parent=render2dp, image="textures/Skunight.png") # Load an image object
-        base.cam2dp.node().getDisplayRegion(0).setSort(-20) 
+        self.background = OnscreenImage(
+            parent=render2dp, image="textures/Skunight.png")  # Load an image object
+        base.cam2dp.node().getDisplayRegion(0).setSort(-20)
         color = (0.8, 0.8, 0.8)
         linfog = Fog("Scene-wide exponential Fog object")
         linfog.setColor(*color)
-        linfog.setLinearRange(0,25)
-        linfog.setLinearFallback(45,160,320)
+        linfog.setLinearRange(0, 25)
+        linfog.setLinearFallback(45, 160, 320)
         render.setFog(linfog)
         base.setBackgroundColor(*color)
         self.plight_lv1 = PointLight("plight")
-        self.plight_lv1.setColor((1,1,1,1))
+        self.plight_lv1.setColor((1, 1, 1, 1))
         self.plnp_lv1 = self.render.attachNewNode(self.plight_lv1)
-        self.plight_lv1.setAttenuation((1,0,1))
-        self.plnp_lv1.setPos(0.33, 6,4)
+        self.plight_lv1.setAttenuation((1, 0, 1))
+        self.plnp_lv1.setPos(0.33, 6, 4)
         self.render.setLight(self.plnp_lv1)
->>>>>>> 41a598963fcc8b82062a41293ac630927ed6bc20
-
 
     def transition(self, task):
         textposx = 0
@@ -125,18 +122,23 @@ class Tijuana2033(ShowBase):
         self.scene.setScale(1, 1, 1)
         self.scene.setPos(0, 25, 0)
         # TITLE TEXT PART...
-        if task.time < 2.0:
+        if len(self.blade_runner.name) < 1:
+            task.time = 0
+            return Task.again
+        if task.time !=0:
             self.textObject = OnscreenText(text="NIVEL 1", pos=(textposx, textposy), scale=0.1,  fg=[
-                                        240, 240, 240, 1], wordwrap=45, bg=[0, 0, 0, 0.1])
-            return Task.cont
+                240, 240, 240, 1], wordwrap=45, bg=[0, 0, 0, 0.1])
+        return Task.done
+        
 
         self.textObject.removeNode()
         return Task.done
-    
-    def remover(self,task):
+
+    def remover(self, task):
         self.textObject.removeNode()
         return task.done
-    def level1(self,task):  
+
+    def level1(self, task):
         self.Nivel1 = self.loader.loadModel("models/Campestre_light.bam")
         self.Nivel1.reparentTo(self.render)
         self.Nivel1.setScale(1, 1, 1)
@@ -147,9 +149,7 @@ class Tijuana2033(ShowBase):
         # Force the rendering to render the background image first (so that it will be put to the bottom of the scene since other models will be necessarily drawn on top)
         base.cam2dp.node().getDisplayRegion(0).setSort(-20)
         return task.done
-        
 
-    
     def setFullScreen(self):
         props = WindowProperties()
         if(self.IsFullScreen == True):
@@ -159,8 +159,10 @@ class Tijuana2033(ShowBase):
             props.fullscreen = True
             self.IsFullScreen = True
         self.win.requestProperties(props)
+        
 
-    def Main_menu(self,task):
+    
+    def Main_menu(self, task):
         self.fullScreenShape = self.loader.loadModel(
             "models/fullscreen_shape.bam")
         self.fullScreenShape.reparentTo(self.render)
@@ -183,6 +185,7 @@ class Tijuana2033(ShowBase):
             text=("*"), scale=.05, pos=(-1, 5, -0.73), command=self.setFullScreen)
         self.InvisibleButton.bind(WITHIN, command=self.mouseOver)
         return task.done
+
 
 Game = Tijuana2033()
 Game.run()
